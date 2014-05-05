9421c1015b52b737b87eeced4020f16776132498
BadRenegade <ykdacd123@gmail.com>
2014-05-05 23:38:04 +0800

    core file, mainly js inheritance

---
+++ js/se_core.js
@@ -0,0 +1,16 @@
+(function(win) {
+	var initializing = false;
+	this.Class = function() {};
+	Class.extend = function(proto) {
+		var _super = this.prototype;
+
+		initializing = true;
+		var protytype = new this();
+		initializing = false;
+
+		for (var name in proto) {
+			prototype[name] = 
+		}
+	}
+
+})(window);
