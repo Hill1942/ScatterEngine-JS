3f30c9fda33af2711e0bf3bd76379218f4d379de
Hill1942 <ykdacd123@gmail.com>
2014-12-11 21:46:19 +0800

    test RenderNormal

--- render/test.js
+++ render/test.js
@@ -42,6 +42,40 @@ function testRenderDepth(canvas, scene, camera, maxDepth) {
     ctx.putImageData(imageData, 0, 0);
 }
 
+function testRenderNormal(canvas, scene, camera, maxDepth) {
+
+    var ctx = canvas.getContext("2d");
+
+    var width = parseInt(canvas.getAttribute("width"));
+    var height = parseInt(canvas.getAttribute("height"));
+
+    ctx.fillStyle = "rgb(0, 0, 0)";
+    ctx.fillRect(0, 0, width, height);
+
+    var imageData = ctx.getImageData(0, 0, width, height);
+    var pixels = imageData.data;
+
+    scene.initialize();
+    camera.initialize();
+    var i = 0;
+    for (var y = 0; y < height; y++) {
+        var sy = 1 - y / height;
+        for (var x = 0; x < width; x++) {
+            var sx = x / width;
+            var ray = camera.generateRay(sx, sy);
+            var result = scene.intersect(ray);
+            if (result.geometry) {
+                pixels[i    ] = (result.normal.x + 1) * 128;
+                pixels[i + 1] = (result.normal.y + 1) * 128;
+                pixels[i + 2] = (result.normal.z + 1) * 128;
+                pixels[i + 3] = 255;
+            }
+            i += 4;
+        }
+    }
+
+    ctx.putImageData(imageData, 0, 0);
+}
 
 
 
