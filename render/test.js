653c82e191e7a68c2c1cd54ed259da0002094765
Hill1942 <ykdacd123@gmail.com>
2014-12-11 21:33:08 +0800

    test RenderDepth

--- render/test.js
+++ render/test.js
@@ -2,28 +2,54 @@
  * Created by kaidi on 14-12-11.
  */
 
-var canvas = document.getElementById("testCanvas");
 
-var ctx = canvas.getContext("2d");
 
-var width = parseInt(canvas.getAttribute("width"));
-var height = parseInt(canvas.getAttribute("height"));
+function testRenderDepth(canvas, scene, camera, maxDepth) {
 
-ctx.fillStyle = "rgb(0, 0, 0)";
-ctx.fillRect(0, 0, width, height);
+    var ctx = canvas.getContext("2d");
 
-var imageData = ctx.getImageData(0, 0, width, height);
-var pixels = imageData.data;
+    var width = parseInt(canvas.getAttribute("width"));
+    var height = parseInt(canvas.getAttribute("height"));
 
-var i = 0;
-for (var y = 0; y < height; y++) {
-    for (var x = 0; x < width; x++) {
-        pixels[i++] = x / width * 255;
-        pixels[i++] = y / height * 255;
-        pixels[i++] = 0;
-        pixels[i++] = 128;
+    ctx.fillStyle = "rgb(0, 0, 0)";
+    ctx.fillRect(0, 0, width, height);
+
+    var imageData = ctx.getImageData(0, 0, width, height);
+    var pixels = imageData.data;
+
+    scene.initialize();
+    camera.initialize();
+    var i = 0;
+    for (var y = 0; y < height; y++) {
+        var sy = 1 - y / height;
+        for (var x = 0; x < width; x++) {
+            var sx = x / width;
+            var ray = camera.generateRay(sx, sy);
+            var result = scene.intersect(ray);
+            if (result.geometry) {
+                var depth = 255 - Math.min(
+                        (result.distance / maxDepth) * 255,
+                        255);
+                pixels[i    ] = depth;
+                pixels[i + 1] = depth;
+                pixels[i + 2] = depth;
+                pixels[i + 3] = 255;
+            }
+            i += 4;
+        }
     }
+
+    ctx.putImageData(imageData, 0, 0);
 }
-ctx.putImageData(imageData, 0, 0);
+
+
+
+
+
+
+
+
+
+
 
 
